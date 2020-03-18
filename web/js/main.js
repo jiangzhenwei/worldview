cc1e9e4c88af5ef0d89f69b24dbab55567109823
Jason Kent <jasontkent@gmail.com>
2020-03-18 08:36:50 -0400

    minor formatting tweaks

--- web/js/main.js
+++ web/js/main.js
@@ -48,6 +48,7 @@ const startTime = new Date().getTime();
 const parameters = util.fromQueryString(window.location.search);
 let { elapsed } = util;
 const errors = [];
+
 // Document ready function
 window.onload = () => {
   if (!parameters.elapsed) {
@@ -58,54 +59,55 @@ window.onload = () => {
   const promise = $.getJSON(configURI);
 
   loadingIndicator.delayed(promise, 1000);
-  promise
-    .done((config) => {
-      config.pageLoadTime = parameters.now
-        ? util.parseDateUTC(parameters.now) || new Date()
-        : new Date();
-      config.palettes = {
-        rendered: {},
-        custom: {},
-      };
-      elapsed('Config loaded', config.now, parameters);
-      // Determine which layers need to be preloaded
-      let layers = [];
-      if (
-        (parameters.l && hasCustomTypePalette(parameters.l))
+  promise.done((config) => {
+    config.pageLoadTime = parameters.now
+      ? util.parseDateUTC(parameters.now) || new Date()
+      : new Date();
+    config.palettes = {
+      rendered: {},
+      custom: {},
+    };
+    elapsed('Config loaded', config.now, parameters);
+    // Determine which layers need to be preloaded
+    let layers = [];
+    if (
+      (parameters.l && hasCustomTypePalette(parameters.l))
         || (parameters.l1 && hasCustomTypePalette(parameters.l1))
-      ) {
-        if (parameters.l && hasCustomTypePalette(parameters.l)) {
-          layers.push(...layersParse12(parameters.l, config));
-        }
-
-        if (parameters.l1 && hasCustomTypePalette(parameters.l1)) {
-          layers.push(...layersParse12(parameters.l1, config));
-        }
-        layers = uniqBy(layers, (layer) => {
-          let str = '';
-          CUSTOM_PALETTE_TYPE_ARRAY.forEach((element) => {
-            str += layer[element] ? layer[element][0] : '';
-          });
-          return layer.id + str;
-        });
+    ) {
+      if (parameters.l && hasCustomTypePalette(parameters.l)) {
+        layers.push(...layersParse12(parameters.l, config));
       }
-      const legacyState = parse(parameters, config, errors);
-      layerValidate(errors, config);
-      preloadPalettes(layers, {}, false).then((obj) => {
-        config.palettes = {
-          custom: obj.custom,
-          rendered: obj.rendered,
-        };
-        render(config, parameters, legacyState);
+
+      if (parameters.l1 && hasCustomTypePalette(parameters.l1)) {
+        layers.push(...layersParse12(parameters.l1, config));
+      }
+      layers = uniqBy(layers, (layer) => {
+        let str = '';
+        CUSTOM_PALETTE_TYPE_ARRAY.forEach((element) => {
+          str += layer[element] ? layer[element][0] : '';
+        });
+        return layer.id + str;
       });
-    })
-    .fail(util.error);
+    }
+    const legacyState = parse(parameters, config, errors);
+    layerValidate(errors, config);
+    preloadPalettes(layers, {}, false).then((obj) => {
+      config.palettes = {
+        custom: obj.custom,
+        rendered: obj.rendered,
+      };
+      render(config, parameters, legacyState);
+    });
+  }).fail(util.error);
 };
 
-const render = (config, parameters, legacyState) => {
+const render = (config, legacyState) => {
+  // TODO transform layer properties as necessary on config here
   config.parameters = parameters;
   debugConfig(config);
-  const models = combineModels(config, legacyState); // Get legacy models
+
+  // Get legacy models
+  const models = combineModels(config, legacyState);
 
   // Get Permalink parse/serializers
   const paramSetup = getParamObject(
@@ -126,7 +128,9 @@ const render = (config, parameters, legacyState) => {
     reducers,
     stateToParams,
   );
-  const middleware = getMiddleware(isDebugMode, locationMiddleware); // Get Various Middlewares
+
+  // Get Various Middlewares
+  const middleware = getMiddleware(isDebugMode, locationMiddleware);
   const store = createStore(
     reducersWithLocation,
     getInitialState(models, config, parameters),
